459d50d8d8b4d17607d174ea0d8d1f662dbcd2f3
XxshiftxX <contact@dev-shift.mr>
2020-06-06 14:58:37 +0900

    eslint 설정 변경
    - DI로 인해 빈 생성자가 자주 생기므로 no-useless-constructor 옵션을 비활성화함.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [2, { args: "none" }],
     'import/no-unresolved': 0,
     'linebreak-style': 0,
+    'no-useless-constructor': 0,
   }
 };
