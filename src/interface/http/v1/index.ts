161b52dca0044f8668b90bcd5f1f0141b00f05f3
XxshiftxX <contact@dev-shift.mr>
2020-06-06 16:14:38 +0900

    HTTP v1 API 기본 라우트 설정

--- src/interface/http/v1/index.ts
+++ src/interface/http/v1/index.ts
@@ -8,6 +8,12 @@ class HttpV1Router implements HttpRouter {
   public readonly routerName: string = '/v1';
 
   public readonly router: Express.Router = Express.Router();
+
+  public constructor() {
+    this.router.get('/', (req, res) => {
+      res.send('Fiasco-Online HTTP API v1 SERVER');
+    });
+  }
 }
 
 export default HttpV1Router;
