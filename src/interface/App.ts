8e4d82cba469afcb1c6273565c744ee646f8adfb
XxshiftxX <contact@dev-shift.mr>
2020-06-06 15:29:46 +0900

    메인 App에 runnable 연동

--- src/interface/App.ts
+++ src/interface/App.ts
@@ -1,8 +1,22 @@
-import { injectable } from 'inversify';
+import { injectable, multiInject } from 'inversify';
+import injectables from '@src/injectables';
+import Runnable from './Runnable';
 
 @injectable()
 export default class App {
-  public run() {
+  private runnables: Runnable[];
+
+  public constructor(
+    @multiInject(injectables.Runnable) runnables: Runnable[],
+  ) {
+    this.runnables = runnables;
+  }
+
+  public async run() {
+    const promises = this.runnables.map((runnable) => runnable.run());
+
+    await Promise.all(promises);
+
     console.log('SERVER STARTED');
   }
 }
