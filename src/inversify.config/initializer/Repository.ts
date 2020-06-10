17bdf3cc09d586bcd1762014d38740f1f835b448
gcshift <gcshift@xinics.com>
2020-06-10 19:02:06 +0900

    UserMongoDBRepository DI 적용

--- src/inversify.config/initializer/Repository.ts
+++ src/inversify.config/initializer/Repository.ts
@@ -4,9 +4,14 @@ import injectables from '../injectables';
 
 import PlaysetRepository from '@src/domain/Playset/repository/Playset.repo';
 import PlaysetMongoDBRepository from '@src/repository/Playset/Playset.repo';
+import UserRepository from '@src/domain/User/repository/User.repo';
+import UserMongoDBRepository from '@src/repository/User/User.repo';
 
 export default (container: Container) => {
   container
     .bind<PlaysetRepository>(injectables.PlaysetRepository)
     .to(PlaysetMongoDBRepository);
+  container
+    .bind<UserRepository>(injectables.UserRepository)
+    .to(UserMongoDBRepository);
 };
